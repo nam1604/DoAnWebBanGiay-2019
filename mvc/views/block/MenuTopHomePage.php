caf804d21f748122710d8b9ec060042854941b9f
quynam <quynamle123@gmail.com>
2019-12-19 02:13:23 +0700

    da xoa file sql va xoa aboutus

--- mvc/views/block/MenuTopHomePage.php
+++ mvc/views/block/MenuTopHomePage.php
@@ -4,7 +4,7 @@
             <a href="./Home"><img class="logo" src="./mvc/views/images/logo.jpg" alt="Logo"></a>
             <ul class="menu">
               <li><a href="./Home">Home</a></li>
-              <li><a href="#">About</a></li>
+              <!-- <li><a href="#">About</a></li> -->
               
               <li class="top-nav-container-menu__flex"><a href="./SessionQuanlyGioHang/giohang"><i class="fas fa-cart-plus"></i>
                 Cart</a></li>
